9708a22f8baf0bc111f304e6a2a6b28d276c196a
Brian Shi <brishi@seas.upenn.edu>
2014-09-01 21:36:42 -0400

    more structure and router

--- app/scripts/models/professor.js
+++ app/scripts/models/professor.js
@@ -2,7 +2,7 @@ define([
 	"jquery",
 	"underscore",
 	"backbone",
-	"vent",			// event aggregator
+	"events",			// event aggregator
 
 	], function ( $, _, Backbone, vent ) { 
 		var Professor = Backbone.Model.extend({ 
---
+++ app/scripts/routes/router.js
@@ -0,0 +1,15 @@
+define([
+	"jquery",
+	"underscore",
+	"backbone",
+	"events",
+
+	], function ( $, _, Backbone, vent ) { 
+		var Router = Backbone.Router.extend({
+			routes: { 
+			},
+		});
+
+		return Router;
+	}
+);
